c058963f709a0e00814ad93735bf8c6d2c6b3008
ptrmarcowski <piotr.marcowski@gmail.com>
2019-12-06 13:01:47 +0100

    5 files modified, 5 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  //styleUrls: [ './app.component.css' ]
+  styles: [`
+    h3 {
+      color: dodgerblue;
+    }
+  
+  `]
+
 })
 export class AppComponent  {
-  name = 'Angular';
-  imie = 'Piotrek';
-  liczba = 24;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HttpModule } from '@angular/http';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule,
+  FormsModule
+  
+   ],
+  declarations: [ AppComponent, 
+  HelloComponent,
+  ServerComponent,
+  ServersComponent 
+  ],
+  bootstrap:    [ AppComponent ],
+  providers: []
 })
 export class AppModule { }
